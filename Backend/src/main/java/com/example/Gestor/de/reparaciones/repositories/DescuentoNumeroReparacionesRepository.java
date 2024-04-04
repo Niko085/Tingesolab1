44ad9c52403847d08615f5af6eefb66803fa2e4b
Niko085 <nicolas.gajardo.p@usach.cl>
2024-04-04 16:59:05 -0300

    update

--- Backend/src/main/java/com/example/Gestor/de/reparaciones/repositories/DescuentoNumeroReparacionesRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.Gestor.de.reparaciones.repositories;
-
-import com.example.Gestor.de.reparaciones.entities.DescuentoNumeroReparacionesEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DescuentoNumeroReparacionesRepository extends JpaRepository<DescuentoNumeroReparacionesEntity, Long> {
-}
--- Backend/src/main/java/com/example/Gestor/de/reparaciones/repositories/HistorialReparacionesRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.Gestor.de.reparaciones.repositories;
-
-import com.example.Gestor.de.reparaciones.entities.HistorialReparacionesEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface HistorialReparacionesRepository extends JpaRepository<HistorialReparacionesEntity, Long> {
-}
